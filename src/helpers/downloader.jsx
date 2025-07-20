b75a36a08a444f6abdad0b0213cdec07452f7efe
vanshika1501 <vanshikadishu15sharma@gmail.com>
2025-07-20 19:33:47 +0530

    resume changes

--- src/helpers/downloader.jsx
+++ src/helpers/downloader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const downloader = () => {
   const link = document.createElement("a");
-  link.href = "/resume.pdf"; // path to your PDF in the public folder
-  link.download = "Vanshika_Sharma_Resume.pdf"; // name to save the file as
+  link.href = "/VanshikaSharma_SoftwareDeveloper_Resume.pdf"; // path to your PDF in the public folder
+  link.download = "VanshikaSharma_SoftwareDeveloper_Resume.pdf"; // name to save the file as
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
